32dfd4dcd8021e6b67e9a44dc49286a51582e2b3
Henrik Hillner <henrik@hillner.fi>
2023-04-24 21:21:51 +0300

    WIP Sensor value display card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ClockCard } from './ClockCard';
+import { SensorDisplayCard } from './SensorDisplayCard';
 import { TrainScheduleCard } from './TrainScheduleCard';
 import { WeatherCard } from './WeatherCard';
 
@@ -12,7 +13,10 @@ export default function Page() {
     <div className="grid grid-cols-2 gap-3 sm:grid-cols-4 lg:grid-cols-4 sm:mx-16 justify-center">
       <div className="col-span-2 grid grid-cols-2 gap-2">
         <ClockCard />
+      </div>  
+      <div className="col-span-2 grid grid-cols-2 gap-2">
         <WeatherCard />
+        <SensorDisplayCard sensorName='Hue temperature sensor 1' />
       </div>  
       {titles.map((t, i) => <TrainScheduleCard title={t} stops={stops[i]} patterns={patterns[i]} key={`hsl-card-${i}`} />)}
     </div>
